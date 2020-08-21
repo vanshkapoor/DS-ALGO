2a418de02fe673b5a9b60b52caaec8829de501f7
vanshkapoor <vanshkapoorvk7@gmail.com>
2020-08-21 09:17:20 +0530

    revise

--- map/traverse.cpp
+++ map/traverse.cpp
@@ -1,24 +1,41 @@
-#include<iostream>
-#include<unordered_map>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
 
 using namespace std;
 
 int main()
 {
-    unordered_map<string, int> map;
-    map["a"]=1;
-    map["b"]=4;
-    map["c"]=2;
+    // unordered_map<string, int> map;
+    // map["a"] = 1;
+    // map["b"] = 4;
+    // map["c"] = 2;
 
-    unordered_map<string,int>::iterator it = map.begin();
-    while(it != map.end())
+    // unordered_map<string, int>::iterator it = map.begin();
+    // while (it != map.end())
+    // {
+    //     cout << "key = " << it->first << "value = " << it->second;
+    //     it++;
+
+    //     // map.erase(it);
+    //     // map.erase(it,it+4); 0,1,2,3,4 deleted
+    // }
+
+    // WAY 2
+    unordered_map<int, int> umap;
+
+    int arr[] = {1, 2, 3, 2, 3, 4, 5, 4, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n; i++)
     {
-        cout<<"key = "<<it->first<<"value = "<<it->second;
-        it++;
-
-
-        // map.erase(it);
-        // map.erase(it,it+4); 0,1,2,3,4 deleted
+        if (umap.find(arr[i]) == umap.end())
+        {
+            umap[arr[i]] = 1;
+        }
+    }
+    for (auto i : umap)
+    {
+        cout << i.first << " - " << i.second << endl;
     }
 
     return 0;
--- oops/oops1.cpp
+++ oops/oops1.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
+#include <iostream>
 #include "teacherclass.cpp"
 
 using namespace std;
 
-class student{
-    public:
+class student
+{
+public:
     int sno;
     char name[20];
 };
-
 
 int main()
 {
@@ -16,20 +16,23 @@ int main()
     student *s2 = new student;
 
     teacher t1;
+    teacher *t2 = new teacher;
     t1.no = 4;
-    cin>>t1.name;
+    cin >> t1.name;
     t1.display();
-    cout<<"age === "<<t1.getNo()<<endl;
-
-
+    cout << "age === " << t1.getNo() << endl;
 
     // allocate to dynamicaly created object
     (*s2).sno = 23;
     // OR
-    s2 -> sno = 1;
+    s2->sno = 1;
 
+    cout << s2->sno << endl;
+    cout << t1.no << endl;
 
-    cout<<s2->sno<<endl;
-    cout<<t1.no;
+    // for teacher
+    t2->setNo(2);
+    cout << t2->getNo();
+
     return 0;
 }
